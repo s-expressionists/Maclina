77f298be76e12a9e777228eefbe48e389c6a0d08
Bike <aeshtaer@gmail.com>
2025-04-30 17:04:47 -0400

    Don't use host literals for externalization tests
    This has been masking a bug. The test supposedly tests vectors with
    fill pointers. However, because the vector with fill pointer was a
    literal, it was being dumped by the host. Some hosts, e.g. SBCL,
    will simplify complex arrays. So Maclina never actually got an
    array with fill pointer to test, and so I didn't notice that our
    externalization of arrays with fill pointers is broken.

--- test/fasl/externalize.lisp
+++ test/fasl/externalize.lisp
@@ -80,27 +80,26 @@
 
 ;;; List of objects to test externalization of.
 (defparameter *externalization-universe*
-  '(12 #.(* 3 most-positive-fixnum)
-    #-sbcl #p"SYS:SRC;CORE;LISPLIST.CC"
-    #-sbcl #p"/usr/share/Adobe/doc/example/android_vm/root/sbin/ls.jar"
-    -17 0 #.(- (* most-negative-fixnum 7) 8)
-    4/3 -3/2 7.6s0 3.2f-4 4.1d8 5.6l23 #c(3.2 4.7) #c(-8 3/2)
-    #\Newline #\7 dribble #:make-load-form #:nonexist
-    #.*package* (machine-instance . 223) #1=(#1# (#1# . #1#) . #1#)
-    #.(make-hash-table) #.(make-hash-table :test #'equal)
-    #.(let ((ht (make-hash-table)))
-        (setf (gethash 4 ht) "booze" (gethash 'hello ht) 'world)
-        ht)
-    #3=(#2=(#3# #2#) #1# . #2#) "Hello, world!" #*11010110101
-    #0a17 #4=#0a#4# #5=#2a((#5# #4#) (#3# #5#)) "畫蛇添足"
-    #.(make-array '(3 2) :element-type 'standard-char
-       :initial-contents '((#\f #\7) (#\Space #\\) (#\. #\%)))
-    #.(make-array '(3 3 3) :adjustable t :initial-element '#:horn)
-    ;; This constant crashes SBCL - see SBCL bug 2038233
-    #-sbcl #.(make-array 2 :displaced-to '#5#)
-    #.(make-array 7 :element-type '(unsigned-byte 3) :fill-pointer 2
-       :initial-element 2)
-    #(2 #:blue #'standard-object "who?" #2a((0 1) (1 0)))))
+  (list 12 (* 3 most-positive-fixnum)
+        #-sbcl #p"SYS:SRC;CORE;LISPLIST.CC"
+        #-sbcl #p"/usr/share/Adobe/doc/example/android_vm/root/sbin/ls.jar"
+        -17 0 (- (* most-negative-fixnum 7) 8)
+        4/3 -3/2 7.6s0 3.2f-4 4.1d8 5.6l23 #c(3.2 4.7) #c(-8 3/2)
+        #\Newline #\7 'dribble '#:make-load-form '#:nonexist
+        *package* '(machine-instance . 223) '#1=(#1# (#1# . #1#) . #1#)
+        (make-hash-table) (make-hash-table :test #'equal)
+        (let ((ht (make-hash-table)))
+          (setf (gethash 4 ht) "booze" (gethash 'hello ht) 'world)
+          ht)
+        '#3=(#2=(#3# #2#) #1# . #2#) "Hello, world!" #*11010110101
+        #0a17 #4=#0a#4# #5=#2a((#5# #4#) (#3# #5#)) "畫蛇添足"
+        (make-array '(3 2) :element-type 'standard-char
+                           :initial-contents '((#\f #\7) (#\Space #\\) (#\. #\%)))
+        (make-array '(3 3 3) :adjustable t :initial-element '#:horn)
+        (make-array 2 :displaced-to '#5#)
+        (make-array 7 :element-type '(unsigned-byte 3) :fill-pointer 2
+                      :initial-element 2)
+        #(2 #:blue #'standard-object "who?" #2a((0 1) (1 0)))))
 
 (5am:test externalize-objects
   (mapc #'test-externalize *externalization-universe*)
